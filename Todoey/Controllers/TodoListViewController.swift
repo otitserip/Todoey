2a5f4bbb5262cb92d87f272216cd77bbe8cc4e12
otitserip <otitserip@gmail.com>
2018-11-23 21:40:20 +0000

    persisting data com corredata working

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,29 +7,42 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = [Item()]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    var selectedCategory : Category? {
+        didSet{
+            loadItems()
+        }
+    }
+    //let defaults = UserDefaults.standard
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
 
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+    let context =  (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        searchBar.delegate = self
       
         loadItems()
-        
-        
-        
+ 
     }
 
     
-    //MARK -Tableview Datasource Methods
+    //MARK: -Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -54,14 +67,19 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    //MARK- Tableview Delegate Methods
+    //MARK: - Tableview Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
+        
+        //delete - tem que ser por estaa ordem
+        //context.delete(itemArray[indexPath.row])
+        //itemArray.remove(at: indexPath.row)
+        
         itemArray[indexPath.row].done =  !itemArray[indexPath.row].done
         
-        //save item plist para actualizar a checkmark
+        //save to context actualizar a checkmark
        saveItems()
         
         //update uitableview
@@ -73,9 +91,29 @@ class TodoListViewController: UITableViewController {
  
     }
     
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            // handle delete (by removing the data from your array and updating the tableview)
+            
+            //delete - tem que ser por estaa ordem
+            context.delete(itemArray[indexPath.row])
+            itemArray.remove(at: indexPath.row)
+            
+            //save to context actualizar a checkmark
+            saveItems()
+            
+            
+            
+        }
+    }
+    
  
     
-    //MARK - Add New Items Section
+    //MARK: - Add New Items Section
     
     @IBAction func addButtomPressed(_ sender: UIBarButtonItem) {
         
@@ -89,8 +127,10 @@ class TodoListViewController: UITableViewController {
             //print(textField.text)
             if !(textField.text?.isEmpty)! {
                 
-                let newItem = Item()
+                let newItem = Item(context: self.context)
                 newItem.title = textField.text!
+                newItem.done = false
+                newItem.parentCategory = self.selectedCategory
                 self.itemArray.append(newItem)
                 
                 //save to item.plist
@@ -108,23 +148,18 @@ class TodoListViewController: UITableViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
-        
-      
-        
+                
     }
+
     
-    
-    
-    
-    //MARK - Save to Item Plist
+    //MARK: - Save to Item Plist
     func saveItems() {
         
-        let encoder = PropertyListEncoder()
+       
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+           try context.save()
         } catch {
-            print("Error Handling plist file: \(error)")
+            print("Error saving context: \(error)")
         }
         
         //update uitableview
@@ -132,23 +167,70 @@ class TodoListViewController: UITableViewController {
         
     }
     
-     //MARK - Load Items
-    func loadItems() {
+    //MARK: - Load Items
+    // with  - external parameters
+    // request -  internal parameter
+    // = - default value, quando nao é usado parametros, assume valor default
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
         
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        if let additionalPredicate  = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
             
-            do {
-              itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print ("Error decoding plist file: \(error)")
-            }
-            
+        } else {
+             request.predicate = categoryPredicate
         }
-
-    }
         
-    
+        
+        do {
+           itemArray = try context.fetch(request)
+        } catch {
+            print ("Error fetching data from context \(error)")
+        }
+        
+      tableView.reloadData()
+        
+    }
+
 
 }
+
+
+//MARK: - Search bar delegate methods
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        
+        loadItems(with: request, predicate: predicate)
+        
+        
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0 {
+            
+            loadItems()
+            
+            //para desaparecer o keyboard
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
+    
+    
+}
+
+
+
+
 
